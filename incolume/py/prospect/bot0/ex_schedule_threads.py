f76f0d02b5920c052052c31a43fd5127a73b1532
Britodfbr <britodfbr@gmail.com>
2023-05-25 08:51:27 -0300

    feat: exemplos com schedule

---
+++ incolume/py/prospect/bot0/ex_schedule_threads.py
@@ -0,0 +1,39 @@
+"""Exemplo baseado em https://youtu.be/PAnrHMQBB-Y"""
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+import schedule
+import time
+import threading
+import logging
+
+
+def fake_task() -> None:
+    msg = 'Tarefa executando na thread {}'.format(threading.current_thread())
+    logging.debug(msg)
+    print(msg)
+
+
+def run_thread(job):
+    job_trhead = threading.Thread(target=job)
+    job_trhead.start()
+
+
+# schedule.frequencia.escala.oque
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+schedule.every(5).seconds.do(run_thread, fake_task)
+
+
+stop = 50
+
+while stop > 0:
+    schedule.run_pending()
+    time.sleep(1)
+    stop -= 1
